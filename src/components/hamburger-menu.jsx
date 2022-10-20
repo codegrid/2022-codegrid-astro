5f66a0c459ed425b765d9628bd51282a247d7c7f
yuto343 <namawasabi1229@gmail.com>
2022-10-21 04:48:41 +0900

    gooberを用いたスタイリング

--- src/components/hamburger-menu.jsx
+++ src/components/hamburger-menu.jsx
@@ -1,21 +1,22 @@
 import { useState } from "preact/hooks";
-import "./hamburger-menu.css";
+import { css } from "goober";
 
 export const HamburgerMenu = ({ links }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <button
-        className='button'
+        className={buttonStyle()}
         type='button'
         onClick={() => setIsOpen((bool) => !bool)}
       >
-        <span className='border'></span>
-        <span className='border'></span>
-        <span className='border'></span>
+        {/* 省略 */}
+        <span className={borderStyle()}></span>
+        <span className={borderStyle()}></span>
+        <span className={borderStyle()}></span>
       </button>
       {isOpen && (
-        <ul className='menu'>
+        <ul className={menuStyle()}>
           {links.map((link) => (
             <li>
               <a href={link.href}>{link.label}</a>
@@ -26,3 +27,32 @@ export const HamburgerMenu = ({ links }) => {
     </div>
   );
 };
+
+const buttonStyle = () => css`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+  position: relative;
+`;
+
+const borderStyle = () => css`
+  width: 30px;
+  height: 1px;
+  background-color: black;
+`;
+
+const menuStyle = () => css`
+  position: absolute;
+  width: 100%;
+  right: 0;
+  left: 0;
+  background-color: blanchedalmond;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  box-sizing: border-box;
+  list-style: none;
+`;
