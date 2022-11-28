663034fcb8424df606df9ab617c8d7bb4cbec987
yuto343 <namawasabi1229@gmail.com>
2022-11-28 09:26:04 +0900

    update src/libs/converter.js

--- src/libs/converter.js
+++ src/libs/converter.js
@@ -2,11 +2,7 @@ export const converter = (_obj) => {
   const obj = {};
   for (const prefix of Object.keys(_obj)) {
     for (const [name, value] of Object.entries(_obj[prefix])) {
-      Object.defineProperty(obj, `${prefix}_${name}`, {
-        value,
-        enumerable: true,
-        writable: true,
-      });
+      obj[`${prefix}_${name}`] = value;
     }
   }
 
