afcc6d235469e3c5858ab5d27ed7824d9153bdc2
yuto343 <namawasabi1229@gmail.com>
2023-02-22 13:54:33 +0900

    change date type from string to date.

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from "astro:content";
 const careers = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.string(),
+    date: z.date(),
   }),
 });
 
